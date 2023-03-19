950a54b17c3c93023d5cdabadce83248ef66871b
charles <spamchar1es0@gmail.com>
2023-03-18 19:41:41 -0700

    Final Product

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
 import keyboard as kb
+import mouse
+import pyautogui
+import time
 
 # Declare agent icons
 sage_icon = sg.Button(image_filename="valorant-agent-icons/1679171250868/Sage_icon.png", key="sage")
@@ -27,13 +30,17 @@ yoru_icon = sg.Button(image_filename="valorant-agent-icons/1679171250868/Yoru_ic
 sg.theme('Black')
 label = sg.Text("Valorant Instalock Resource", font=("Valorant",40) ,text_color="white", justification='center')
 sublabel = sg.Text("The all complete tool you need for your instalock"
-                   " necessities!", font=("Valorant", 15), text_color="white", justification='center')
+                   " necessities!", font=("Valorant", 15),
+                   text_color="white", justification='center')
+subsublabel = sg.Text("(Positions your mouse to the desired agent) "
+                      "Requires 1920x1080 resolution and all agents unlocked.", font=("Valorant", 12),
+                      text_color="white", justification='center')
 hotkey_button = sg.Button("Enter key to toggle instalock:", key="key")
 hotkey_label = sg.Text("Current keybind: ", text_color="white"), \
     sg.Text("", font=("Valorant", 40), key="output", text_color="white")
 agent_label = sg.Text("Select an agent:"), sg.Text("", font=("Valorant", 50), key="agent_output", text_color="white")
 
-layout = [[label], [sublabel], [sg.Text("", size=(2, 1))], [hotkey_button], [sg.Text("", size=(2, 1))], [hotkey_label],
+layout = [[label], [sublabel], [subsublabel], [sg.Text("", size=(2, 1))], [hotkey_button], [sg.Text("", size=(2, 1))], [hotkey_label],
           [sg.Text("", size=(2, 1))], [agent_label],
           [astra_icon, breach_icon, brimstone_icon, chamber_icon, cypher_icon, fade_icon, gekko_icon, harbor_icon,
            jett_icon, kayo_icon],
@@ -45,7 +52,15 @@ layout = [[label], [sublabel], [sg.Text("", size=(2, 1))], [hotkey_button], [sg.
 window = sg.Window('You nerd',
                    layout=layout,
                    font=('Valorant', 20),
-                   size=(980, 700))
+                   size=(980, 730))
+
+mouseX = 0
+mouseY = 0
+
+def hotkey_pressed():
+    print(mouse.get_position())
+    pyautogui.moveTo(mouseX, mouseY)
+
 
 while True:
     event, values = window.read()
@@ -54,50 +69,90 @@ while True:
     elif event == 'key':
         key_pressed = kb.read_key()
         window["output"].update(key_pressed)
+        kb.add_hotkey(key_pressed, hotkey_pressed)
     else:
         match event:
             case "sage":
                 window["agent_output"].update("Sage")
             case "astra":
                 window["agent_output"].update("Astra")
+                mouseX = 539
+                mouseY = 918
             case "breach":
                 window["agent_output"].update("Breach")
+                mouseX = 622
+                mouseY = 918
             case "brimstone":
                 window["agent_output"].update("Brimstone")
+                mouseX = 717
+                mouseY = 918
             case "chamber":
                 window["agent_output"].update("Chamber")
+                mouseX = 786
+                mouseY = 918
             case "cypher":
                 window["agent_output"].update("Cypher")
+                mouseX = 876
+                mouseY = 918
             case "fade":
                 window["agent_output"].update("Fade")
+                mouseX = 960
+                mouseY = 918
             case "gekko":
                 window["agent_output"].update("Gekko")
+                mouseX = 1044
+                mouseY = 918
             case "harbor":
                 window["agent_output"].update("Harbor")
+                mouseX = 1122
+                mouseY = 918
             case "jett":
                 window["agent_output"].update("Jett")
+                mouseX = 1212
+                mouseY = 918
             case "kayo":
                 window["agent_output"].update("kayo")
+                mouseX = 1287
+                mouseY = 918
             case "killjoy":
                 window["agent_output"].update("Killjoy")
+                mouseX = 1380
+                mouseY = 918
             case "neon":
                 window["agent_output"].update("Neon")
+                mouseX = 546
+                mouseY = 1000
             case "omen":
                 window["agent_output"].update("Omen")
+                mouseX = 626
+                mouseY = 1000
             case "phoenix":
                 window["agent_output"].update("Phoenix")
+                mouseX = 717
+                mouseY = 1000
             case "raze":
                 window["agent_output"].update("Raze")
+                mouseX = 797
+                mouseY = 1000
             case "reyna":
                 window["agent_output"].update("Reyna")
+                mouseX = 872
+                mouseY = 1000
             case "skye":
                 window["agent_output"].update("Skye")
+                mouseX = 969
+                mouseY = 1000
             case "sova":
                 window["agent_output"].update("Sova")
+                mouseX = 1040
+                mouseY = 1000
             case "viper":
                 window["agent_output"].update("Viper")
+                mouseX = 1132
+                mouseY = 1000
             case "yoru":
                 window["agent_output"].update("Yoru")
-
-
+                mouseX = 1221
+                mouseY = 1000
+    time.sleep(0.3)
 window.close()
